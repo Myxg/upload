a1a918817fc72f4b5c071b4962d9f96c87973c0b
myxg <15234407153@163.com>
2020-08-18 15:29:56 +0800

    yici

---
+++ upload/myapp/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, redirect
+import os
+import time
+import datetime
+
+
+# Create your views here.
+
+
+def demo(request):
+    if request.method == 'POST':
+        try:
+            myFile = request.FILES.get("myfile",None)
+            # if not myFile:
+            #     return HttpResponse('no files for upload!')
+
+            # report.objects.filter(id=uid).update(filename=myFile.name)
+            # # print(uid)
+            # ts = report.objects.filter(id=uid)
+            # for i in ts:
+            #     s = i.route
+            #     route = '/SHreport' + s
+            now = int(time.time())
+            ts = time.localtime(now)
+            t = str(ts.tm_year)+str(ts.tm_mon).zfill(2)+str(ts.tm_mday).zfill(2)
+            route = 'D:\\data\\2020\\0817\\0000\\video\\'
+            file = t + '_' + myFile.name
+            destination = open(os.path.join(route,file),'wb+')
+
+            for chunk in myFile.chunks():
+                destination.write(chunk)
+                # print(destination,'----------------------')
+            destination.close()
+            list1 = ['上传成功']
+            return render(request, 'demo.html', {'msg':'上传成功'})
+        except Exception as e:
+            return render(request, 'demo.html', {'msg': '上传失败'})
+    return render(request, 'demo.html')
+
+
